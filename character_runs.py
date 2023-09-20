b474cefe1cb9ec18d71e2bfd257026f438789419
Tkadha(uni) <emforhs228@naver.com>
2023-09-20 17:44:14 +0900

    큰 틀 생성

--- character_runs.py
+++ character_runs.py
@@ -5,7 +5,20 @@ open_canvas()
 
 # fill here
 
+grass = load_image('grass.png')
+character = load_image('character.png')
 
+def run_circle():
+    print('circle')
+    pass
+def run_rectangle():
+    print('rectangle')
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+    break
     
 
 close_canvas()
