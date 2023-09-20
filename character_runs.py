0f856c8a4a1207e40aa736e7cc8bdf3dbcd9ac9c
Tkadha(uni) <emforhs228@naver.com>
2023-09-20 18:22:57 +0900

    move right line

--- character_runs.py
+++ character_runs.py
@@ -31,11 +31,16 @@ def run_rectangle():
     print('rectangle')
 
     # bottom line
-    #for x in range(50,750+1,10):
+    # for x in range(50,750+1,10):
     #   render_all(x,90)
+
+    # right line
+    for y in range(90,550+1,10):
+        render_all(750,y)
+    
     # top line
-    for x in range(750,50-1,-10):
-        render_all(x,550)
+    # for x in range(750,50-1,-10):
+    #   render_all(x,550)
     
     pass
 
